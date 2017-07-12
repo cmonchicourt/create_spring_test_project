c4c3fe501f8c603a1afe7910c3491a1aaf5de225
Cédric Monchicourt <cmonchicourt@micropole.com>
2017-07-12 21:08:59 +0200

    first commit

---
+++ src/main/java/com/create_project/create_project/CreateProjectApplication.java
@@ -0,0 +1,12 @@
+package com.create_project.create_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateProjectApplication.class, args);
+	}
+}
